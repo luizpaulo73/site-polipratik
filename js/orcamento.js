5e66c30bb1d881eefdab1ff78f3a280d3d5df942
luizpaulo73 <luiz.paulo2005@hotmail.com>
2024-06-28 00:04:17 -0300

    site com responsividade e section orcamento adicionada

--- js/orcamento.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-    var loadingScreen = document.getElementById('loading-screen');
-    var content = document.getElementById('content');
-
-    setTimeout(function() {
-        loadingScreen.style.display = 'none';
-        content.style.display = 'block';
-    }, 2500);
-});
